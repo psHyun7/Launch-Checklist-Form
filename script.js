10ecadf9b731ca1bb6c926d303f5b9469bd8ba5f
Sung Hyun Park <sunghyun.park92@gmail.com>
2020-05-26 02:00:26 -0400

    completed updating shuttle requirements

--- script.js
+++ script.js
@@ -18,12 +18,37 @@ window.addEventListener("load", function() {
       let copilotName = document.querySelector("input[name=copilotName]");
       let fuelLevel = document.querySelector("input[name=fuelLevel");
       let cargoMass = document.querySelector("input[name=cargoMass]");
-      
+
       console.log(pilotName)
       console.log(pilotName.value)
       if (!pilotName.value || !copilotName.value || !fuelLevel.value || !cargoMass.value) {
          alert("Make sure to enter valid information for each field!")
          event.preventDefault();
       }
+
+      let pilotStatus = document.getElementById("pilotStatus");
+      let copilotStatus = document.getElementById("copilotStatus");
+      let fuelStatus = document.getElementById("fuelStatus");
+      let cargoStatus = document.getElementById("cargoStatus");
+
+      let faultyItemList = document.getElementById("faultyItems");
+      let launchStatus = document.getElementById("launchStatus");
+
+      if (parseInt(fuelLevel.value) < 10000) {
+        faultyItemList.style.visibility = "visible";
+        fuelStatus.innerHTML = "There is not enough fuel for the journey";
+        launchStatus.style.color = "red";
+        launchStatus.innerHTML = "Shuttle not ready for launch";
+      }
+      if (parseInt(cargoMass.value) > 10000) {
+        faltyItemList.style.visibility = "visible";
+        cargoStatus.innerHTML = "There is too much mass for shuttle to lift off";
+        launchStatus.style.color = "red";
+        launchStatus.innerHTML = "Shuttle not ready for launch";
+      }
+
+      launchStatus.style.color = "green";
+      launchStatus.innerHTML = "Shuttle ready for launch";
+
    });
 });
