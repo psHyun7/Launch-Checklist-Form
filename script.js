5bb58ce5af38b3289f78d7caa599174280a93766
SungHyun <sunghyun.park92@gmail.com>
2020-05-26 01:13:06 -0400

    Added Validation

--- script.js
+++ script.js
@@ -11,3 +11,19 @@
 </ol>
 <img src="${}">
 */
+window.addEventListener("load", function() {
+   let submitButton = document.getElementById("formSubmit");
+   submitButton.addEventListener("click", function(event) {
+      let pilotName = document.querySelector("input[name=pilotName]");
+      let copilotName = document.querySelector("input[name=copilotName]");
+      let fuelLevel = document.querySelector("input[name=fuelLevel");
+      let cargoMass = document.querySelector("input[name=cargoMass]");
+      
+      console.log(pilotName)
+      console.log(pilotName.value)
+      if (!pilotName.value || !copilotName.value || !fuelLevel.value || !cargoMass.value) {
+         alert("Make sure to enter valid information for each field!")
+         event.preventDefault();
+      }
+   });
+});
